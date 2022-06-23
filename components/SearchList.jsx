a8961272d6e9393e770d8262c3fe82dc8380c2b2
anugoyal998 <anugoyal998@gmail.com>
2022-06-23 19:52:17 +0530

    api

--- components/SearchList.jsx
+++ components/SearchList.jsx
@@ -3,17 +3,19 @@ import React from "react";
 import IMG from "./img";
 
 function SearchList({ data }) {
-  console.log(data);
   if (data?.length <= 0) return <div></div>;
   return (
     <div className="absolute z-50 bg-gray-100 w-full top-16 -left-2 shadow-md  py-3 rounded-sm">
       {data?.map((e) => {
         return (
-          <Link href={`/post/${e?._id}`} key={e?._id}>
+          <Link
+            href={`/post/${e?._id}`}
+            key={e?._id}
+          >
             <div className="flex items-center p-3 space-x-3 cursor-pointer hover:bg-slate-200">
               <img src={e?.postImage ? e?.postImage : IMG} className="w-12" />
               <div>
-                <Link href={`subreddit/${e?.subreddit}`}>
+                <Link href={`/subreddit/${e?.subreddit}`}>
                   <p className="font-bold hover:text-blue-400 ">
                     r/{e?.subreddit}
                   </p>
--- pages/api/post/search.js
+++ pages/api/post/search.js
@@ -4,12 +4,14 @@ import PostModel from "../../../models/post.model";
 const handler = async (req, res) => {
   try {
     const { search } = req.body;
-    const response = await PostModel.find({
-      subreddit: search,
-      title: search,
-      body: search,
-    });
-    res.status(200).json(response)
+    let response = await PostModel.find({});
+    response = response.filter(
+      (e) =>
+        e?.postTitle?.toLowerCase()?.includes(search.toLowerCase()) ||
+        e?.postBody?.toLowerCase()?.includes(search.toLowerCase()) ||
+        e?.subreddit?.toLowerCase()?.includes(search.toLowerCase())
+    );
+    res.status(200).json(response);
   } catch (error) {
     console.log(error);
     return res.status(500).json({ data: "error" });
